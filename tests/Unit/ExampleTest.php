b0b95157caafa5153db9f395278ca49553bd0495
jose  julian <202213865h@utea.edu.pe>
2026-05-25 22:41:08 -0500

    feat: agregadas las primeras  pruebas del test Unit

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -10,6 +10,6 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true(): void
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
     }
 }
